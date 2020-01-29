ac3f934880fd37564b98ac3b4b195161e592befb
CelesteMagisteel <celestemagisteel@gmail.com#>
2020-01-29 21:49:35 +0000

    Added update description

--- database/migrations/2020_01_27_103813_create_plugin_versions_table.php
+++ database/migrations/2020_01_27_103813_create_plugin_versions_table.php
@@ -17,6 +17,7 @@ class CreatePluginVersionsTable extends Migration
             $table->bigIncrements('id');
             $table->bigInteger('plugin_id');
             $table->string('version');
+            $table->text('update_description')->nullable(true);
             $table->timestamps();
         });
     }
