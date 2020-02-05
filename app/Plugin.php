9a0eb15ec1278b137d64b2b9369dcbf0ab32858f
CelesteMagisteel <celestemagisteel@gmail.com#>
2020-02-05 13:55:54 +0000

    Removed Server model deemed out-of-scope

--- app/Plugin.php
+++ app/Plugin.php
@@ -7,11 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Plugin extends Model
 {
 
-
-    public function server() {
-        return $this->belongsTo('App\Server');
-    }
-
     public function versions() {
         return $this->hasMany('App\PluginVersion');
     }
--- app/Server.php
+++ app/Server.php
@@ -6,10 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Server extends Model
 {
-    public function plugins() {
-        return $this->hasMany('App\Plugin');
-    }
-
     public function owner() {
         return $this->belongsTo('App\User');
     }
