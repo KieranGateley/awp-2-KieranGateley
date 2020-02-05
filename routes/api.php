f00d33b7d93b0b6c9767877c63817ee13d9737c3
CelesteMagisteel <celestemagisteel@gmail.com#>
2020-02-05 15:28:11 +0000

    Added api routes for plugins

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,17 @@
 <?php
 
 use Illuminate\Http\Request;
+Route::group(['middleware' => 'auth:api'], function () {
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+    Route::get('plugins', 'ApiPluginController@index');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::get('plugins/{id}', 'ApiPluginController@show');
+
+    Route::get('plugins/create', 'ApiPluginController@store');
+
+    Route::get('plugins/update/{id}', 'ApiPluginController@update');
+
+    Route::get('plugins/delete/{id}', 'ApiPluginController@delete');
+
 });
+
