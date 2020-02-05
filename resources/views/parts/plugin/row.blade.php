ce977e98c53dc4b5fdfed295f4005dec79227fe6
CelesteMagisteel <celestemagisteel@gmail.com#>
2020-02-05 13:23:18 +0000

    Added plugin row view

---
+++ resources/views/parts/plugin/row.blade.php
@@ -0,0 +1,8 @@
+<tr>
+    <th scope="row">{{ $name }}</th>
+    <td>{{ $description }}</td>
+    <td>{{ $authors }}</td>
+    <td>{{ $website }}</td>
+    <td>{{ $dependencies }}</td>
+    <td>{{ $soft_dependencies }}</td>
+</tr>
