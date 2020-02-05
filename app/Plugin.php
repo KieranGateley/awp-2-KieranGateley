d39923844637bcc8993125727eaa034e4bb93e41
CelesteMagisteel <celestemagisteel@gmail.com#>
2020-02-05 14:05:18 +0000

    Set fillable fields on models

--- app/Plugin.php
+++ app/Plugin.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Plugin extends Model
 {
+    protected $fillable = ['name', 'description', 'authors', 'website', 'dependencies', 'soft_dependencies'];
 
     public function versions() {
         return $this->hasMany('App\PluginVersion');
--- app/PluginVersion.php
+++ app/PluginVersion.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class PluginVersion extends Model
 {
+    protected $fillable = ['plugin_id', 'version', 'update_description'];
 
     public function plugin() {
         return $this->belongsTo('App\Plugin');
