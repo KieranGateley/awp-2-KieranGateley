821260f62c4eaf0c0dbad8c0aad3b0e46612ab8c
CelesteMagisteel <celestemagisteel@gmail.com#>
2020-02-05 13:39:12 +0000

    Implemented all plugins view

---
+++ resources/views/content/plugin/table.blade.php
@@ -0,0 +1,33 @@
+@extends('bases.container')
+
+@section('title', 'All Users')
+
+@section('scripts')
+    $(document).ready(function(){ $('#plugin').DataTable(); });
+@endsection
+
+@section('content')
+    <table id="plugins" style="width: 100%;" class="table">
+        <thead class="thead-dark">
+        <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+            <th scope="col">Authors</th>
+            <th scope="col">Website</th>
+            <th scope="col">Dependencies</th>
+            <th scope="col">Soft Dependencies</th>
+        </tr>
+        </thead>
+        @foreach ($plugins as $plugin)
+            @include('parts.plugin.row', [
+                        'name' => $plugin->name,
+                        'description' => $plugin->description,
+                        'authors' => $plugin->authors,
+                        'website' => $plugin->website,
+                        'dependencies' => $plugin->dependencies,
+                        'soft_dependencies' => $plugin->soft_dependencies,
+                    ])
+        @endforeach
+    </table>
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,9 @@ Route::get('/', function () {
     return view('content.homepage');
 })->name('home');
 
+Route::get('/plugins', function () {
+    return view('content.plugin.table', ['plugins' => App\Plugin::all()]);
+})->name('all_plugins');
+
 Auth::routes();
 
