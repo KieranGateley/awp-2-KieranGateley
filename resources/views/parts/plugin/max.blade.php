89b27e9a15349f43c2ebb5515ab4cc46ba73e04b
Celeste Magisteel <kamg1134@gmail.com>
2020-02-07 09:28:45 +0000

    Made max plugin display a list of versions

--- resources/views/parts/plugin/max.blade.php
+++ resources/views/parts/plugin/max.blade.php
@@ -7,4 +7,18 @@
     @isset($dependencies)<em>Dependencies: </em>{{ $dependencies }}<br>@endisset
     @isset($soft_dependencies)<em>Soft-Dependencies: </em>{{ $soft_dependencies }}<br>@endisset
     @isset($description)<p></p><div id="title">{{ $description }}</div>@endisset
+    <p></p>
+    <table id="versions" style="width: 100%;" class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col" style="width: 20%">Version</th>
+                <th scope="col">Description</th>
+            </tr>
+        </thead>
+        @isset($versions)
+            @foreach($versions as $version)
+                @include('parts.plugin.version.row', ['version' => $version->version, 'description' => $version->description])
+            @endforeach
+        @endisset
+    </table>
 </div>
