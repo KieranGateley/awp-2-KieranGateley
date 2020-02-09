f015db63379028a90fea7515ac9969ef67e7dcc3
CelesteMagisteel <celestemagisteel@gmail.com#>
2020-02-09 12:57:59 +0000

    Added version web routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,15 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/plugins/update/{plugin}', 'PluginController@edit')->name('edit_plugin');
 
     Route::post('/plugins/update/{plugin}', 'PluginController@update')->name('edit_plugin');
+
+    Route::get('/plugins/{plugin}/versions/create', 'PluginVersionController@create')->name('create_version');
+
+    Route::post('/plugins/{plugin}/versions/create', 'PluginVersionController@store')->name('create_version');
+
+    Route::get('/plugins/{plugin}/versions/update/{version}', 'PluginVersionController@edit')->name('edit_version');
+
+    Route::post('/plugins/{plugin}/versions/update/{version}', 'PluginVersionController@update')->name('edit_version');
+
 });
 
 Route::get('/', function () {
@@ -20,6 +29,10 @@ Route::get('/', function () {
 Route::get('/plugins', 'PluginController@index')->name('all_plugins');
 
 Route::get('/plugins/{plugin}', 'PluginController@show')->name('view_plugin');
+
+Route::get('/versions', 'PluginVersionController@index')->name('all_versions');
+
+Route::get('/plugins/{plugin}/versions/{version}', 'PluginVersionController@show')->name('view_version');
 
 Auth::routes();
 
