1aaec8a503f6bd0730159415cd4d13220eebda2a
CelesteMagisteel <celestemagisteel@gmail.com#>
2020-02-09 13:14:12 +0000

    Fixed legacy field name in version fillable

--- app/PluginVersion.php
+++ app/PluginVersion.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class PluginVersion extends Model
 {
-    protected $fillable = ['plugin_name', 'version', 'update_description'];
+    protected $fillable = ['plugin_name', 'version', 'description'];
 
     public function plugin() {
         return $this->belongsTo('App\Plugin', 'plugin_name');
