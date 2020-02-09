229bb33ba25d526a776164f60b9554b57a82364c
CelesteMagisteel <celestemagisteel@gmail.com#>
2020-02-09 12:50:35 +0000

    Implemented PluginVersionController with default redirects

---
+++ app/Http/Controllers/PluginVersionController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Plugin;
+use App\PluginVersion;
+use Illuminate\Http\Request;
+
+class PluginVersionController extends Controller {
+
+    public function index() {
+        return view('content.version.table', ['versions' => PluginVersion::all()]);
+    }
+
+    public function create(Plugin $plugin)  {
+        return view('content.version.create', ['plugin' => $plugin]);
+
+    }
+
+    public function store(Request $request, Plugin $plugin) {
+        $version = PluginVersion::create([
+            'version' => $request->input('version'),
+            'description' => $request->input('description'),
+        ]);
+        $plugin->versions()->save($version);
+        return redirect()->route('home');
+    }
+
+    public function show(Plugin $plugin, PluginVersion $version) {
+        return view('content.version.view', ['plugin' => $plugin, 'version' => $version]);
+    }
+
+
+    public function edit(Plugin $plugin, PluginVersion $version) {
+        return view('content.version.update', ['plugin' => $plugin, 'version' => $version]);
+    }
+
+    public function update(Request $request, Plugin $plugin, PluginVersion $version) {
+        $version->update([
+            'version' => $request->input('version'),
+            'description' => $request->input('description'),
+        ]);
+        return redirect()->route('home');
+    }
+
+    public function destroy(Plugin $plugin, PluginVersion $version) {
+        $version->delete();
+        return redirect()->route('home');
+    }
+}
