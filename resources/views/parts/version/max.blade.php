505adb71534e946667cc3aeedcebab55c73a66e3
CelesteMagisteel <celestemagisteel@gmail.com#>
2020-02-09 12:40:32 +0000

    Implemented max version view

---
+++ resources/views/parts/version/max.blade.php
@@ -0,0 +1,7 @@
+<div id="title" class="border-bottom">
+    <h1 style="display: inline-block;">{{ $name }}&#64;{{ $version }}</h1>
+</div>
+<div id="body">
+    @isset($description){{ $description }}<br>@endisset
+    <p></p>
+</div>
