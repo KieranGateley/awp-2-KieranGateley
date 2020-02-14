bfeb7bdd6dadf5b3d485765807ea8f730f58dbb3
CelesteMagisteel <celestemagisteel@gmail.com#>
2020-02-14 20:33:34 +0000

    Added new links to navbar

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark border-bottom text-light">
-    <div class="navbar-brand abs" href="#"><a class="text-light" href="{{ route('home') }}">Spigot Plugin Manager</a></div>
+    <div class="navbar-brand abs"><a class="text-light" href="{{ route('home') }}">Spigot Plugin Manager</a></div>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -7,6 +7,12 @@
     <div class="navbar-collapse collapse" id="collapsingNavbar">
         <ul class="nav navbar-nav ml-auto">
             @if (Auth::check())
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('create_plugin') }}">Create Plugin</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('all_plugins') }}">View Plugins</a>
+                </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout</a>
                 </li>
