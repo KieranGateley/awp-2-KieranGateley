18d7189c876bbc0e31e598ff393f9acd02f18b0c
CelesteMagisteel <celestemagisteel@gmail.com#>
2020-03-02 10:29:00 +0000

    Added version min view

---
+++ resources/views/content/version/min.blade.php
@@ -0,0 +1,12 @@
+<div class="version" style="border: 2px solid black; border-radius: 5px; padding-left: 5px; padding-right: 5px">
+    <div id="header" class="border-bottom"><b>{{ $name }}&amp;{{ $version }}</b></div>
+    <div id="body">{{ Str::words($description, $limit=50, $end='...') }}</div>
+    <div id="footer" class="border-top">
+        <div id="info" class="text-left" style="width: 70%; display: inline-block">
+            Update Created {{ $date }}
+        </div>
+        <div class="text-right" style="width: 29%; display: inline-block">
+            <a href="{{ route('view_plugin', ['plugin' => $plugin, ]) }}">Read more...</a>
+        </div>
+    </div>
+</div>
