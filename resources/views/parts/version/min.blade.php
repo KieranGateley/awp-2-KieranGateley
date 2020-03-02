9ef2a7c062a39346ba537b3ba0586eb58b6fc378
CelesteMagisteel <celestemagisteel@gmail.com#>
2020-03-02 10:38:56 +0000

    Added default values to min blade

--- resources/views/parts/version/min.blade.php
+++ resources/views/parts/version/min.blade.php
@@ -1,6 +1,6 @@
 <div class="version" style="border: 2px solid black; border-radius: 5px; padding-left: 5px; padding-right: 5px">
-    <div id="header" class="border-bottom"><b>{{ $name }}&amp;{{ $version }}</b></div>
-    <div id="body">{{ Str::words($description, $limit=50, $end='...') }}</div>
+    <div id="header" class="border-bottom"><b>{{ $name }}&#64;{{ $version }}</b></div>
+    <div id="body">{{ Str::words($description, $limit=50, $end='...') ?? "" }}</div>
     <div id="footer" class="border-top">
         <div id="info" class="text-left" style="width: 70%; display: inline-block">
             Update Created {{ $date }}
