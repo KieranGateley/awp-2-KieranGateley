f1900aa010e8852942fb5fd773da827adb8a736d
CelesteMagisteel <celestemagisteel@gmail.com#>
2020-03-09 10:25:01 +0000

    Changed title to capitalize the first letter of all words

--- database/factories/PluginFactory.php
+++ database/factories/PluginFactory.php
@@ -7,8 +7,13 @@ use Faker\Generator as Faker;
 use Illuminate\Database\Eloquent\Factory;
 
 $factory->define(Plugin::class, function (Faker $faker) {
+    $words = $faker->words($nbWords = 3);
+    $title = "";
+    foreach ($words as $word) {
+        $title = $title . ucfirst($word);
+    }
     return [
-        'name' => implode('', $faker->words($nbWords = 3)),
+        'name' => $title,
         'description' => $faker->realText(600),
         'authors' => $faker->userName,
         'website' => 'http://' . $faker->domainName,
