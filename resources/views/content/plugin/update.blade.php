445113a57715c4f45609a7e2a6e74dbdfd9bd91f
CelesteMagisteel <kamg1134@gmail.com>
2020-04-27 10:39:57 +0100

    Fixed spigot id not being properly input to the plugin update form

--- resources/views/content/plugin/update.blade.php
+++ resources/views/content/plugin/update.blade.php
@@ -9,6 +9,7 @@
         'description' => $plugin->description,
         'authors' => $plugin->authors,
         'website' => $plugin->website,
+        'spigot_id' => $plugin->spigot_id,
         'dependencies' => $plugin->dependencies,
         'soft_dependencies' => $plugin->soft_dependencies,
     ])
